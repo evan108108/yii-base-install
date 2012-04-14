cdff29386ed18a4b607bfa75671f286be720175e
Evan Frohlich <evan.frohlich@controlgroup.com>
2012-04-14 18:41:42 -0400

    Removing packages from build

--- webapp/protected/vendors/crisu83/bootstrap/demo/protected/views/site/_thumb.php
+++
@@ -1,5 +0,0 @@
-<li class="span3">
-	<a href="#" class="thumbnail" rel="tooltip" data-title="Tooltip">
-		<img src="http://placehold.it/280x180" alt="">
-	</a>
-</li>
--- webapp/protected/vendors/generator4fixtures/gii/gii/fixture/FixtureGenerator.php
+++
@@ -1,1 +0,0 @@
-<?phpclass FixtureGenerator extends CCodeGenerator{    public $codeModel='application.gii.fixture.FixtureCode';}
